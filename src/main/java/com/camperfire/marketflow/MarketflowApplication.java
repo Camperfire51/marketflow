d9b30f5e7873731069bb556e0d7a628ce77032bf
camperfire <camperfire51@gmail.com>
2024-12-21 12:36:53 +0300

    Remove redundant code

--- src/main/java/com/camperfire/marketflow/MarketflowApplication.java
+++ src/main/java/com/camperfire/marketflow/MarketflowApplication.java
@@ -13,29 +13,6 @@ public class MarketflowApplication {
 	public static final Map map = new HashMap<Product, Long>();
 
 	public static void main(String[] args) {
-
-		Product product = new Product();
-
-		product.setName("Banana");
-
-		map.put(product, 1L);
-
-		product.setName("Apple");
-
-		System.out.println(map.get(product));
-
-		String someString = new String("Some String") ;
-		String someString2 = new String("Some String") ;
-
-		System.out.println(someString.equals(someString2));
-
-
-        Product product1 = new Product();
-        Product product2 = new Product();
-
-        System.out.println(product1.hashCode());
-        System.out.println(product2.hashCode());
-
 		SpringApplication.run(MarketflowApplication.class, args);
 
 
